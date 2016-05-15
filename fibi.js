960832901c9301441ee89192295f9cb2f6c9343b
gunbladesniper <savaughnjones@gmail.com>
2016-05-15 17:22:30 -0400

    Update fibi.js
    added function

--- fibi.js
+++ fibi.js
@@ -1,16 +1,25 @@
-# Fibi
+
 var fib = [];
 var fibstart = prompt("Pick a number between 1 and 10.");
+  fib[0]= 1;
+  fib[1]= 1;
 
-fib[0]= 1;
-fib[1]= 1;
+
+var fibNum = function(){
+
 for(var count = 2; count <101; count++){
   fib[count] = fib[count-2] + fib[count-1];
 };
 for (var i = 0; i < fibstart; i++){
 
   console.log(fib[i]);
-
 };
+};
+fibNum();
+
+
 
 //var answer = prompt("What is the next number?");
+  
+
+
