87377c037a56a4ff5da09e74d6848abfcb903d08
gunbladesniper <savaughnjones@gmail.com>
2016-07-16 20:23:00 -0400

    Update asynch.js

--- asynch.js
+++ asynch.js
@@ -1,14 +1,16 @@
-//function combines takes 2 async functions that have no arguments except a
-// callback and the third argument is a callback. it calls the third callback
-//with null or error for the first argument and the second is the callback value
-// from the first function. third argument is the value from the second function.
-//fn((cb)=>cb(null, 1), (cb)=>cb(null,3),(err,a,b)
 
 
 
 function combine(af1, af2, clbk){
-   clbk.call(this, null, af1, af2);
-   
+  var a = af1.call(null, function(num){
+    return num;
+  });
+  var b = af2.call(null, function(num){
+    return num;
+  });
+
+  clbk.call(this, null, a, b);
+
 }
 
 function callback(err, a, b){
@@ -18,4 +20,16 @@ function callback(err, a, b){
     console.log(a+b);
 }
 
-combine(1, 3, callback);
+function uno(clbk){
+  return clbk.call(null, 1);
+
+}
+function tres(clbk){
+  return clbk.call(null, 3);
+  
+}
+function numb(num){
+  return num;
+};
+
+combine(uno, tres , callback);
